dfece571dd30fe8e71660ff0baf47c40d26611b9
alxn787 <alenabraham787@gmail.com>
2025-01-27 16:07:56 +0530

    ui change

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -9,18 +9,17 @@ export const Hero = () => {
     const router = useRouter();
     return (
         <div className="flex flex-col items-center bg-gradient-to-r from-[#090808] via-[#0c111f] to-[#0d111f] text-white min-h-screen py-12">
-            <div className="text-6xl font-semibold text-center">
-                <span>The Indian Cryptocurrency </span>
-                <span className="text-blue-400">Revolution</span>
-            </div>
-            <div className="text-gray-400 text-2xl mt-6 text-center">
-                Create a frictionless wallet with just a Google Account
-            </div>
-            <div className="text-gray-400 text-2xl mt-6 text-center">
-                Trade tokens with ease
-            </div>
+            <div className="py-10 text-center">
+          <h1 className="text-6xl font-bold mb-6">
+            Wallet as a Service,
+            <span className="text-[rgb(74,118,230)]"> Simplified</span>
+          </h1>
+          <div className="text-xl text-gray-300 max-w-2xl mx-auto">
+                Swap tokens and manage balances with ease.
+          </div>
+        </div>
             {session.data?.user ? (
-                <div className="mt-8 text-white py-2 px-6 rounded shadow-lg">
+                <div className=" text-white py-2 px-6 rounded shadow-lg">
                     <Button
                         insidevalue="Go to Dashboard"
                         onClick={() => router.push("/dashboard")}                    />
@@ -29,6 +28,10 @@ export const Hero = () => {
                 <div className="mt-8 text-white py-2 px-6 rounded shadow-lg">
                     <Button
                         insidevalue="Sign Up With Google"
+                        onClick={() => signIn("google")}
+                    />
+                    <Button
+                        insidevalue="View Demo"
                         onClick={() => signIn("google")}
                     />
                 </div>
