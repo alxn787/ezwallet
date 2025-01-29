3d0b28ec31b8b6ec680dd8d7c63d4ad15922c684
alxn787 <alenabraham787@gmail.com>
2025-01-29 20:51:37 +0530

    color change

--- app/components/Appbar.tsx
+++ app/components/Appbar.tsx
@@ -5,7 +5,7 @@ import { Button } from "./Button";
 export default function Appbar(){
     const session = useSession();
     return (
-        <div className=" top-0 z-50 p-2  flex justify-between text-white font-extrabold text-xl px-20 left-0 right-0 fixed backdrop-blur-sm bg-gradient-to-r from-[#080d18] via-[#0c111f] to-[#0d111f]">
+        <div className=" top-0 z-50 p-2  flex justify-between text-white font-extrabold text-xl px-20 left-0 right-0 fixed backdrop-blur-sm bg-gradient-to-r from-[#090808] via-[#0c111f] to-[#0d111f]">
             <div>
                 EZWALLET
             </div>
